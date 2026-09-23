66c4dc4c9c4e0e497ea0444e97bc59d857c3366f
no-life-tech-nerd <2604171@sit.singaporetech.edu.sg>
2026-09-24 03:07:39 +0800

    feat: move logic into get_valid_input

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,33 +1,35 @@
 from logging import warning
 
-inventory = int(0)
+inventory = float(0)
 
-userInput = ""
-
-errorCount = int(0)
+errorCount = float(0)
 
 def warnUser(message: str) -> None:
     global errorCount
     warning(message)
     errorCount += 1
 
-while userInput != "quit":
-    userInput = input("Enter a stock quantity: ")
-    if userInput == "quit":
-        print("Goodbye!")
-        break
-    try:
-        addedInventory = int(userInput)
-        if addedInventory < 0:
-            warnUser("Negative number not accepted, try again!")
-        else:
-            inventory = int(inventory) + int(addedInventory)
-    except:
-        warnUser(f"Input of type {str(type(userInput))} not accepted, try again!")
-        continue
-    if inventory > 500:
-        print("Inventory has overstocked!")
-        break
+def get_value_input() -> float:
+    userInput = ""
+    while userInput != "quit":
+        userInput = input("Enter a stock quantity: ")
+        if userInput == "quit":
+            print("Goodbye!")
+            break
+        try:
+            addedInventory = float(userInput)
+            if addedInventory < 0:
+                warnUser("Negative number not accepted, try again!")
+            else:
+                return float(addedInventory)
+        except:
+            warnUser(f"Input of type {str(type(userInput))} not accepted, try again!")
+            continue
+    return 0
 
-print(f"Total Units Processed: {str(inventory)}")
-print(f"Number of Failed/Rejected Entries: {str(errorCount)}")
+# def process_delivery(current_total, new_value) -> float:
+#     return
+#
+# def calculate_tax(amount) -> float:
+#
+# def generate_report(total_units, failed_attempts) -> str:
